e4b12234d6e6917e022cdfc7f549f738d1df6f87
aritejhg <E0425486@u.nus.edu>
2023-02-01 18:46:13 +0800

    main update, standardised, added thresh, led check

--- scripts/data_mm_analyse.py
+++ scripts/data_mm_analyse.py
@@ -4,24 +4,26 @@ import glob
 import os
 import argparse
 from pathlib import Path
-from scripts.dist_vel_acc import vel_acc, distance
+from scripts.dist_vel_acc import distance_vel, acc
 
 from scripts.data_integrity import check_overall, jittery_frames
 
 def convert_mm(name, df, debug):
+    print(f"Converting: {name} to mm") if debug else None
+
     # define X and Y columns to apply the different conversion
-    x_columns = ["Tail_x", "Body1_x", "Body2_x", "Body3_x", "Head_x"]
-    y_columns = ["Tail_y", "Body1_y", "Body2_y", "Body3_y", "Head_y"]
+    x_columns = [col for col in df.columns if "_x" in col]
+    y_columns = [col for col in df.columns if "_y" in col]
 
     # Convert to mm
-    df[x_columns] = df[x_columns].astype(float)* 0.28
-    df[y_columns] = df[y_columns].astype(float) * -0.304
+    df[x_columns] = df[x_columns] * 0.28
+    df[y_columns] = df[y_columns] * -0.304
 
-    print(f"Converted: {name} to mm")
+    print(df.head(5)) if debug else None
     return df
 
 
-def main(csv_path, percent_missing_thresh, invalid_thresh, output_path, debug):
+def main(csv_path, LED_csv, percent_missing_thresh, percent_low_likelihood_thresh, invalid_thresh, sd_threshold, output_path, debug):
     csv_files = list(csv_path.glob("**/*.csv"))
 
     problems_csv = []
@@ -37,63 +39,92 @@ def main(csv_path, percent_missing_thresh, invalid_thresh, output_path, debug):
         # Read csv and create proper columns
         df = pd.read_csv(csv_file, header = None)
         print(csv_file.name)
+        
+        # get led time of the csv file
+        led_row = LED_csv[LED_csv["name"].str.contains(csv_file.stem)]
+        start_frame, end_frame = led_row["start frame"].values[0] - 1, led_row["end frame"].values[0] - 1 # -1 because of 0 indexing
+
         df.columns = df.iloc[2] + "_" + df.iloc[3] # naming the columns as Tail_x, Tail_y, Tail_likelihood, etc.
-        df.drop([0,1,2,3], inplace=True) # remove the model name and row that just said individual
+        df.drop([0,1,2,3], inplace=True) # remove the first 4 rows. Contains model name, individual name, body part and x/y/likelihood
         df.reset_index(drop=True, inplace=True) # resets the row index
         print(df.head(5)) if debug else None
         
+        # convert to numeric dtype from str
+        df = df.apply(pd.to_numeric, downcast = 'float' ,errors='coerce')
+
         # Convert to mm
         df = convert_mm(csv_file.name, df, debug)
         print(df.head(5)) if debug else None
 
         #check overall data integrity
-        percent_missing = check_overall(df, invalid_thresh, debug)
-        print(f"{percent_missing:.2f}% missing data")
+        percent_missing, percent_low_likelihood = check_overall(df, invalid_thresh, debug)
+        print(f"{percent_missing:.2f}% missing data, {percent_low_likelihood:.2f}% low likelihood data")
         
         if percent_missing > percent_missing_thresh:
             print(f"Too much missing data in {csv_file.name}")
-            problems_csv.append((csv_file.name, percent_missing))
+            problems_csv.append([csv_file.name, percent_missing, "missing"])
+        if percent_low_likelihood > percent_low_likelihood_thresh:
+            print(f"Too much low likelihood data in {csv_file.name}")
+            problems_csv.append([csv_file.name, percent_low_likelihood, "low_likelihood"])
         
-        print("checking for invalid frames") if debug else None
-        # replace <bodypart>_likelihood with 0 depending on if it is invalid or not.
+        # print("checking for invalid frames") if debug else None
+        # replace <bodypart>_likelihood with 0 depending on if it is invalid or not. NOT NEEDED RN.
         # invalid frames are frames where the likelihood is less than the invalid_thresh or null
         likelihood_columns = [col for col in df.columns if "likelihood" in col]
-        df[likelihood_columns] = df[likelihood_columns].apply(lambda x: x.where(x.astype(float) >= invalid_thresh, 0).fillna(0))
+        # df[likelihood_columns] = df[likelihood_columns].apply(lambda x: x.where(x.astype(float) >= invalid_thresh, 0).fillna(0))
         
-        print("calculating distance travelled") if debug else None
         # need distance travelled to do jitter analysis
-        df = distance(df, debug)
+        df = distance_vel(df, debug)
 
-        print("checking for jittery frames") if debug else None
         #check for jittery frames
-        jittery = jittery_frames(df, debug)
-        df["jittery"] = 0 # add a jittery column
-        df.loc[jittery, "jittery"] = 1
+        df = jittery_frames(df, sd_threshold, debug)
 
-        # do further analysis on the data
-        df = vel_acc(df, fps, debug)
+        # check for invalid/jittery frames in LED timing
+        mask_led = df.loc[start_frame:end_frame, likelihood_columns] < 0.25
+        # led_invalid_frames = df[mask_led]
+        mask_jitter = df.loc[start_frame:end_frame, "jittery"] == 1
+        # led_jittery_frames = df[mask_jitter]
+        led_invalid_or_jittery_frames = df.loc[start_frame:end_frame][(mask_led).any(axis=1) | mask_jitter]
+        print(led_invalid_or_jittery_frames.index.values)
+        # if len(led_invalid_frames) > 0:
+        #     print(f"Invalid frames in {csv_file.name} during LED timing")
+        #     problems_csv.append([csv_file.name, led_invalid_frames.index.values, "invalid during LED"])
+        # if len(led_jittery_frames) > 0:
+        #     print(f"Jittery frames in {csv_file.name} during LED timing")
+        #     problems_csv.append([csv_file.name, led_jittery_frames.index.values, "jittery during LED"])
+
+        # getting acceleration
+        df = acc(df, fps, debug) 
         
-        print(df.head(5)) if debug else None
-
-        # Save to output path
-        os.makedirs(output_path, exist_ok=True)
-        df.to_csv(output_path / csv_file.name, index=False)
+        # Save to output path to the correct subdirectory path
+        output_csv_path = output_path / csv_file.relative_to(csv_path)
+        os.makedirs(output_csv_path.parent, exist_ok=True)
+        df.to_csv(output_csv_path, index=False)
+        print(f"Saved to {output_csv_path}")
     np.savetxt(output_path / "problems_csv.txt", problems_csv, delimiter=",", fmt="%s")
 
 if __name__ == "__main__":
     # Parser
     parser = argparse.ArgumentParser()
     parser.add_argument("--csv_path", default = "./data/output/subset/", help="the filepath to subset csv files if not ./data/output/subset/")
+    parser.add_argument("--LED_csv_path", default = "./data/output/LED_times.csv", help="the filepath to LED csv file if not ./data/output/LED_times.csv")
     parser.add_argument("--output_path", default = "./data/output/mm_analyse", help="the filepath to output csv if not ./data/output/mm_analyse")
     parser.add_argument("--debug", action="store_true", default = False, help="debug mode (default is false)")
     parser.add_argument("--invalid_thresh", default = 0.25, help="the threshold for invalid frames (default is 0.25)")
-    parser.add_argument("--percent_missing", default = 15, help="the threshold for percent missing data (default is 15%)")
+    parser.add_argument("--percent_missing", default = 10, help="the threshold for percent missing data (default is 10%)")
+    parser.add_argument("--percent_low_likelihood", default = 15, help="the threshold for percent low likelihood data (default is 15%)")
+    parser.add_argument("--two_sd_thresh", default = 88.44, help="the threshold for 2SD of body part (default is 88.44)")
 
     args = parser.parse_args()
     csv_path = Path(args.csv_path)
+    LED_csv_path = Path(args.LED_csv_path)
     output_path = Path(args.output_path)
     invalid_thresh = args.invalid_thresh
     percent_missing_thresh = args.percent_missing
+    percent_low_likelihood_thresh = args.percent_low_likelihood
+    sd_threshold = args.two_sd_thresh
     debug = args.debug
 
-    main(csv_path, percent_missing_thresh, invalid_thresh, output_path, debug)
+    LED_csv = pd.read_csv(LED_csv_path)
+
+    main(csv_path, LED_csv, percent_missing_thresh, percent_low_likelihood_thresh, invalid_thresh, sd_threshold, output_path, debug)
--- scripts/dist_vel_acc.py
+++ scripts/dist_vel_acc.py
@@ -3,33 +3,33 @@ import numpy as np
 from scipy.spatial.distance import pdist
 
 
-def distance(df, debug):
+def distance_vel(df, debug):
     """ calculate the distance travelled between frames for each body part"""
+    print("Calculating distance (same as vel)") if debug else None
+
     # define X and Y columns to apply the different conversion
-    x_columns = ["Tail_x", "Body1_x", "Body2_x", "Body3_x", "Head_x"]
-    y_columns = ["Tail_y", "Body1_y", "Body2_y", "Body3_y", "Head_y"]
+    x_columns = [col for col in df.columns if "_x" in col]
+    y_columns = [col for col in df.columns if "_y" in col]
+    likelihood_columns = [col for col in df.columns if "likelihood" in col]
+
     for (x,y) in zip(x_columns, y_columns): #iterate over body part
         # caluclate the distance between last frame and current frame for bodypart
         # column name is {bodypart}_dist
-        df[f'{x.split("_")[0]}_dist'] = np.sqrt((df[x].diff())**2 + (df[y].diff())**2)          
+        df[f'{x.split("_")[0]}_dist'] = np.sqrt((df[x].diff())**2 + (df[y].diff())**2)
+    weights = df[likelihood_columns] #.astype(float)
+    #using masked average to avoid null values
+    df["avg_dist_vel"] = np.average( df[[col for col in df.columns if "dist" in col]], axis=1, weights = weights)          
+    
     print(df.head(5)) if debug else None
     return df
 
-def vel_acc(df, fps, debug):
+def acc(df, fps, debug):
     """the velocity and acceleration are calculated using the distance columns"""
-
-    print("Calculating velocity and acceleration") if debug else None
-    
     # fps 
+    print("Calculating acceleration") if debug else None
     time_per_frame = 1/fps
-    # calculate the velocity using distance columns
-    for column in df.columns:
-        if "dist" in column:
-            # calculate the velocity using distance columns
-            # split column name to get the body part name
-            df[f'{column.split("_")[0]}_vel'] = df[column] / time_per_frame
-            # calculate the acceleration using velocity columns
-            df[f'{column.split("_")[0]}_acc'] = df[f'{column.split("_")[0]}_vel'] / time_per_frame
+
+    df["avg_acc"] = df["avg_dist_vel"] / time_per_frame
 
     print(df.head(5)) if debug else None
     return df
