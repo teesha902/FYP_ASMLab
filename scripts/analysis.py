348af5f835b7c1d71c59d62cdf38839ad28cd421
aritejhg <E0425486@u.nus.edu>
2023-02-21 17:39:15 +0800

    analysis, surface angle 360, surface jitter

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -56,6 +56,8 @@ def angle(df, debug):
     x_columns = ["Head_x", "Tail_x"]
     y_columns = ["Head_y", "Tail_y"]
     df["angle"] = np.arctan2(df[y_columns[0]] - df[y_columns[1]], df[x_columns[0]] - df[x_columns[1]]) * 180 / np.pi
+    # convert the negative angles to positive
+    df["angle"].apply(lambda x: x if x >= 0 else 360 + x) # its positive because the angle itself is negative
     print(df.head(5)) if debug else None
     return df
 
@@ -64,12 +66,15 @@ def distance_surface(df, debug):
     surface is set to the highest y coordinate in the frames
     this outputs a column with averaged y distance of body parts from averaged ymax
     calculations use min and subtract from max to work with flipped y axis"""
+
     print ("Calculating distance from surface") if debug else None
 
     y_columns = [col for col in df.columns if "_y" in col]
     # surface = df[y_columns].max(axis=1).average
     surface = df[y_columns].max(axis=0).mean()
     # df["distance_surface"] = surface - df[y_columns].average(axis=0)
-    df["distance_surface"] = df[y_columns].mean(axis=1) - surface
+    df["distance_surface"] = (df[y_columns].mean(axis=1) - surface)
+    # set negative values to nan and mark as jitter in the jitter column
+    df.loc[df["distance_surface"] < 0, ["distance_surface", "jitter"]] = [Null, 1]
     print(df.head(5)) if debug else None
     return df
