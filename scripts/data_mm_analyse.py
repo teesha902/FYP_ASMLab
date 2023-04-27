8e60cc33714befe78d91ecdb106839a7dd53d480
aritejhg <E0425486@u.nus.edu>
2023-04-27 16:31:31 +0800

    ran d77 + analysis try-except

--- scripts/data_mm_analyse.py
+++ scripts/data_mm_analyse.py
@@ -60,92 +60,101 @@ def main(csv_path, LED_csv, percent_missing_thresh, percent_low_likelihood_thres
     fps = 30 # TODO: get fps from csv file
 
     # Iterate through all csv files
+
     for csv_file in csv_files:
-        # Read csv and create proper columns
-        df = pd.read_csv(csv_file, header = None)
-        print(csv_file.name)
-        
-        # get led time of the csv file
-        led_row = LED_csv[LED_csv["name"].str.contains(csv_file.stem)]
-        print(led_row) if debug else None
-        led_absent = False # reset led_absent flag
-        if led_row.empty:
-            print(f"LED file not found for {csv_file.name}")
-            problems_csv.append([csv_file.name, "LED file not found"])
-            led_absent = True
-        if not led_absent:
-            start_frame, end_frame = led_row["start frame"].values[0] - 1, led_row["end frame"].values[0] - 1 # -1 because of 0 indexing
-            led_x = led_row["x"].values[0] * 0.28 # convert to mm
-            led_y = led_row["y"].values[0] * -0.304 # convert to mm
-        df.columns = df.iloc[2] + "_" + df.iloc[3] # naming the columns as Tail_x, Tail_y, Tail_likelihood, etc.
-        df.drop([0,1,2,3], inplace=True) # remove the first 4 rows. Contains model name, individual name, body part and x/y/likelihood
-        df.reset_index(drop=True, inplace=True) # resets the row index
-        print(df.head(5)) if debug else None
-        
-        # convert to numeric data type from str
-        df = df.apply(pd.to_numeric, downcast = 'float' ,errors='coerce')
 
-        # Convert to mm
-        df = convert_mm(csv_file.name, df, debug)
-        print(df.head(5)) if debug else None
+        try:
 
-        #check overall data integrity
-        percent_missing, percent_low_likelihood = check_overall(df, invalid_thresh, debug)
-        print(f"{percent_missing:.2f}% missing data, {percent_low_likelihood:.2f}% low likelihood data")
-        
-        if percent_missing > percent_missing_thresh:
-            print(f"Too much missing data in {csv_file.name}")
-            problems_csv.append([csv_file.name, percent_missing, "missing"])
-        if percent_low_likelihood > percent_low_likelihood_thresh:
-            print(f"Too much low likelihood data in {csv_file.name}")
-            problems_csv.append([csv_file.name, percent_low_likelihood, "low_likelihood"])
-        
-        # replace <bodypart>_likelihood with 0 depending on if it is invalid or not. NOT NEEDED RN.
-        # invalid frames are frames where the likelihood is less than the invalid_thresh or null
-        likelihood_columns = [col for col in df.columns if "likelihood" in col]
-        # df[likelihood_columns] = df[likelihood_columns].apply(lambda x: x.where(x.astype(float) >= invalid_thresh, 0).fillna(0))
-        
-        # need distance travelled to do jitter analysis
-        df = distance_vel(df, debug)
+            # Read csv and create proper columns
+            df = pd.read_csv(csv_file, header = None)
+            print(csv_file.name)
+            
+            # get led time of the csv file
+            led_row = LED_csv[LED_csv["name"].str.contains(csv_file.stem)]
+            print(led_row) if debug else None
+            led_absent = False # reset led_absent flag
+            if led_row.empty:
+                print(f"LED file not found for {csv_file.name}")
+                problems_csv.append([csv_file.name, "LED file not found"])
+                led_absent = True
+            if not led_absent:
+                start_frame, end_frame = led_row["start frame"].values[0] - 1, led_row["end frame"].values[0] - 1 # -1 because of 0 indexing
+                led_x = led_row["x"].values[0] * 0.28 # convert to mm
+                led_y = led_row["y"].values[0] * -0.304 # convert to mm
+            df.columns = df.iloc[2] + "_" + df.iloc[3] # naming the columns as Tail_x, Tail_y, Tail_likelihood, etc.
+            df.drop([0,1,2,3], inplace=True) # remove the first 4 rows. Contains model name, individual name, body part and x/y/likelihood
+            df.reset_index(drop=True, inplace=True) # resets the row index
+            print(df.head(5)) if debug else None
+            
+            # convert to numeric data type from str
+            df = df.apply(pd.to_numeric, downcast = 'float' ,errors='coerce')
 
-        #check for jittery frames
-        df = jittery_frames(df, sd_threshold, debug)
+            # Convert to mm
+            df = convert_mm(csv_file.name, df, debug)
+            print(df.head(5)) if debug else None
 
-        # check for invalid/jittery frames in LED timing
-        if not led_absent:
-            print("checking for invalid frames in LED timing") if debug else None
+            #check overall data integrity
+            percent_missing, percent_low_likelihood = check_overall(df, invalid_thresh, debug)
+            print(f"{percent_missing:.2f}% missing data, {percent_low_likelihood:.2f}% low likelihood data")
+            
+            if percent_missing > percent_missing_thresh:
+                print(f"Too much missing data in {csv_file.name}")
+                problems_csv.append([csv_file.name, percent_missing, "missing"])
+            if percent_low_likelihood > percent_low_likelihood_thresh:
+                print(f"Too much low likelihood data in {csv_file.name}")
+                problems_csv.append([csv_file.name, percent_low_likelihood, "low_likelihood"])
+            
+            # replace <bodypart>_likelihood with 0 depending on if it is invalid or not. NOT NEEDED RN.
+            # invalid frames are frames where the likelihood is less than the invalid_thresh or null
+            likelihood_columns = [col for col in df.columns if "likelihood" in col]
+            # df[likelihood_columns] = df[likelihood_columns].apply(lambda x: x.where(x.astype(float) >= invalid_thresh, 0).fillna(0))
+            
+            # need distance travelled to do jitter analysis
+            df = distance_vel(df, debug)
+
+            #check for jittery frames
+            df = jittery_frames(df, sd_threshold, debug)
+
             # check for invalid/jittery frames in LED timing
-            mask_low_likelihood = df.loc[start_frame:end_frame, likelihood_columns] < invalid_thresh
-            mask_absent = df.loc[start_frame:end_frame, likelihood_columns].isnull()
-            combined = mask_low_likelihood | mask_absent
-            print (combined.head(5)) if debug else None
-            mask = combined.sum(axis=1) >= 4 # if more than 2 columns have low likelihood or absent
-            print(mask.head(5)) if debug else None
-            if len(combined.index[mask].tolist())/(end_frame-start_frame) > led_absent_thresh:
-                print(f"{csv_file.name} contains > {led_absent_thresh * 100}% invalid frames in LED timing")
-                problems_csv.append([csv_file.name, "invalid frames during LED", combined.index[mask].tolist()])
+            if not led_absent:
+                print("checking for invalid frames in LED timing") if debug else None
+                # check for invalid/jittery frames in LED timing
+                mask_low_likelihood = df.loc[start_frame:end_frame, likelihood_columns] < invalid_thresh
+                mask_absent = df.loc[start_frame:end_frame, likelihood_columns].isnull()
+                combined = mask_low_likelihood | mask_absent
+                print (combined.head(5)) if debug else None
+                mask = combined.sum(axis=1) >= 4 # if more than 2 columns have low likelihood or absent
+                print(mask.head(5)) if debug else None
+                if len(combined.index[mask].tolist())/(end_frame-start_frame) > led_absent_thresh:
+                    print(f"{csv_file.name} contains > {led_absent_thresh * 100}% invalid frames in LED timing")
+                    problems_csv.append([csv_file.name, "invalid frames during LED", combined.index[mask].tolist()])
 
-            mask_jitter = df.loc[start_frame:end_frame, "jittery"] == 1
-            if len(mask_jitter.index[mask_jitter].tolist())/(end_frame - start_frame) > led_jitter_thresh:
-                print(f" >10% Jittery frames in {csv_file.name} during LED timing")
-                problems_csv.append([csv_file.name, "jittery frames during LED", mask_jitter.index[mask_jitter].tolist()])
+                mask_jitter = df.loc[start_frame:end_frame, "jittery"] == 1
+                if len(mask_jitter.index[mask_jitter].tolist())/(end_frame - start_frame) > led_jitter_thresh:
+                    print(f" >10% Jittery frames in {csv_file.name} during LED timing")
+                    problems_csv.append([csv_file.name, "jittery frames during LED", mask_jitter.index[mask_jitter].tolist()])
 
-            #distance to surface
-            df = distance_led(df, led_y, debug)
-
-
-        # getting acceleration
-        df = acc(df, fps, debug) 
+                #distance to surface
+                df = distance_led(df, led_y, debug)
 
 
-        # angle using tail and head
-        df = tail_head_angle(df, debug)
-        
-        # Save to output path to the correct subdirectory path
-        output_csv_path = output_path / csv_file.relative_to(csv_path)
-        os.makedirs(output_csv_path.parent, exist_ok=True)
-        df.to_csv(output_csv_path, index=False)
-        print(f"Saved to ./{output_csv_path}")
+            # getting acceleration
+            df = acc(df, fps, debug) 
+
+
+            # angle using tail and head
+            df = tail_head_angle(df, debug)
+            
+            # Save to output path to the correct subdirectory path
+            output_csv_path = output_path / csv_file.relative_to(csv_path)
+            os.makedirs(output_csv_path.parent, exist_ok=True)
+            df.to_csv(output_csv_path, index=False)
+            print(f"Saved to ./{output_csv_path}")
+        except Exception as e:
+            print("----------------EXCEPTION--------------------")
+            print(e)
+            print("---------------------------------------------")
+            continue
 
     print("Done")
     unique_csv = np.unique([i[0] for i in problems_csv])
@@ -161,6 +170,8 @@ def main(csv_path, LED_csv, percent_missing_thresh, percent_low_likelihood_thres
             f.write(f'{item} \n')
 
     # np.savetxt(output_path / "problems_csv.txt", problems_csv, delimiter=",", fmt="%s")
+
+        
 
 if __name__ == "__main__":
     # Parser
